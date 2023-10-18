c0dc0c8b0a057492b498db37edbde61b001f6c05
Nagesh Kumar Mishra <nagesh.mishra@fissionlabs.com>
2023-10-18 16:09:54 +0530

    - Dashboard design is updated

--- mobile/ios/x/Dashboard/DashboardView.swift
+++ mobile/ios/x/Dashboard/DashboardView.swift
@@ -11,7 +11,7 @@ import SwiftData
 struct DashboardView: View {
     @Environment(\.modelContext) private var modelContext
     
-    @State private var latencyText = "256 ms"
+    @State private var latencyText = "256"
     @State private var elapaedTime = "01:01:13.211"
     @State private var voiceDecibel = "42 / 64 dB"
     @State private var paymentsCredits = "8.067 ONE"
@@ -20,17 +20,17 @@ struct DashboardView: View {
     @State private var sessionIdentifier = "1234-5678-90ab"
     
     var body: some View {
-        
-        Color.black.ignoresSafeArea()
+        Color(hex:0x313131).ignoresSafeArea()
             .overlay(
                 VStack {
                     HStack {
-                        Text(elapaedTime).font(.customFont())
+                        Text(elapaedTime)
+                            .font(.customFont())
                             .foregroundColor(.white)
                         Spacer()
                         Text(voiceDecibel){ text in
                             text.font = .customFont()
-                            text.foregroundColor = Color(hex: 0x479CCC)
+                            text.foregroundColor = Color(hex: 0x00AEE9)
                             if let range = text.range(of: "/"),
                                let dbRange = text.range(of: "dB") {
                                 text[range].foregroundColor = Color.white
@@ -40,47 +40,54 @@ struct DashboardView: View {
                     }
                     .padding(.top, 30)
                     Spacer()
-                    Text(latencyText){ text in
-                        text.font = .customFont(size: 64)
-                        text.foregroundColor = Color(hex: 0x479CCC)
-                        if let range = text.range(of: "ms") {
-                            text[range].font = .customFont(size: 32)
-                            text[range].foregroundColor = Color.white
+                    
+                    HStack {
+                        VStack {
+                            Text(latencyText){ text in
+                                text.font = .customFont(size: 84)
+                                text.foregroundColor = Color(hex: 0x00AEE9)
+                            }
                         }
+                        Text("ms"){ text in
+                            text.font = .customFont(size: 54)
+                            text.foregroundColor = Color.white
+                        }.padding(.top, 10)
                     }
                     Spacer()
                     HStack {
                         Text(paymentsCredits){ text in
                             text.font = .customFont()
-                            text.foregroundColor = Color(hex: 0x479CCC)
+                            text.foregroundColor = Color(hex: 0x00AEE9)
                             
                             if let range = text.range(of: "ONE"){
                                 text[range].foregroundColor = Color.white
                             }
                         }
                         Spacer()
-                        
                         Text(optimiseValue){ text in
                             text.font = .customFont()
-                            text.foregroundColor = Color(hex: 0x479CCC)
+                            text.foregroundColor = Color(hex: 0x00AEE9)
                             for each in text.characters.ranges(of: "+"){
                                 text[each].foregroundColor = Color.white
                             }
                         }
                     }
                     HStack {
-                        Text(source).font(.customFont())
+                        Text(source)
+                            .font(.customFont())
                             .foregroundColor(.white)
                         Spacer()
-                        Text(sessionIdentifier).font(.customFont())
+                        Text(sessionIdentifier)
+                            .font(.customFont())
                             .foregroundColor(.white)
                     }
                     .padding(.bottom, 30)
                 }
                     .padding(10)
+            ).onAppear(
+                perform: SpeechRecognition().setup
             )
     }
-    
 }
 
 #Preview {
