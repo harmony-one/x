415775ef585d3db424b4aebccfb264e7348e22be
ahiipsa <ahiipsa@gmail.com>
2023-10-18 16:39:07 +0400

    Added vocode_backend

---
+++ demo/vocode-backend/main.py
@@ -0,0 +1,37 @@
+import logging
+from fastapi import FastAPI
+
+from vocode.streaming.models.agent import ChatGPTAgentConfig
+from vocode.streaming.models.synthesizer import AzureSynthesizerConfig
+from vocode.streaming.synthesizer.azure_synthesizer import AzureSynthesizer
+
+from vocode.streaming.agent.chat_gpt_agent import ChatGPTAgent
+from vocode.streaming.client_backend.conversation import ConversationRouter
+from vocode.streaming.models.message import BaseMessage
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI(docs_url=None)
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+conversation_router = ConversationRouter(
+    agent_thunk=lambda: ChatGPTAgent(
+        ChatGPTAgentConfig(
+            initial_message=BaseMessage(text="Hello!"),
+            prompt_preamble="Have a pleasant conversation about life",
+        )
+    ),
+    synthesizer_thunk=lambda output_audio_config: AzureSynthesizer(
+        AzureSynthesizerConfig.from_output_audio_config(
+            output_audio_config, voice_name="en-US-SteffanNeural"
+        )
+    ),
+    logger=logger,
+)
+
+app.include_router(conversation_router.get_router())
