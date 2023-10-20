77b38749f8cd1285a4a818313f56a0fe542b0279
artemkolodko <temakolodko@gmail.com>
2023-10-20 10:44:32 +0100

    Remove specific rule for first sentence

--- demo/webapp/src/stores/ChatGptStore/helpers.ts
+++ demo/webapp/src/stores/ChatGptStore/helpers.ts
@@ -2,10 +2,5 @@ export const isPhraseComplete = (text: string, isFirst = false) => {
     const wordsAmount = text.split(' ').length;
 
     const specSymbols = ['?', '.', '!'];
-
-    if (isFirst) {
-        return wordsAmount > 30 || [...specSymbols, ','].some(symbol => text.includes(symbol));
-    } else {
-        return wordsAmount > 40 || specSymbols.some(symbol => text.includes(symbol));
-    }
+    return wordsAmount > 40 || specSymbols.some(symbol => text.includes(symbol));
 }
