267e50860233b406e81f9d8b4a48f6520bb062d4
potvik <yuriy@harmony.one>
2023-10-23 20:16:12 +0300

    IOS Emotion: added project structure

---
+++ mobile/samantha/x/Extension/Text.swift
@@ -0,0 +1,18 @@
+//
+//  Text.swift
+//  x
+//
+//  Created by Nagesh Kumar Mishra on 16/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+/// extension to make applying AttributedString even easier
+extension Text {
+    init(_ string: String, configure: ((inout AttributedString) -> Void)) {
+        var attributedString = AttributedString(string) /// create an AttributedString
+        configure(&attributedString) /// configure using the closure
+        self.init(attributedString) /// initialize a Text
+    }
+}
