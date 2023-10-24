9cb7db81f062c216f244a53596e3f016ea8b3690
Theo Fandrich <tf@harmony.one>
2023-10-24 09:18:34 -0700

    added theo

---
+++ mobile/theo/x/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  x
+//
+//  Created by Aaron Li on 10/13/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date = Date()
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ mobile/theo/x/OpenAIService/Models/Choices.swift
@@ -0,0 +1,28 @@
+//
+//  Choices.swift
+//  x
+//
+//  Created by Nagesh Kumar Mishra on 18/10/23.
+//
+
+import Foundation
+struct Choices : Codable {
+	let index : Int?
+	let message : Message?
+	let finish_reason : String?
+
+	enum CodingKeys: String, CodingKey {
+
+		case index = "index"
+		case message = "message"
+		case finish_reason = "finish_reason"
+	}
+
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		index = try values.decodeIfPresent(Int.self, forKey: .index)
+		message = try values.decodeIfPresent(Message.self, forKey: .message)
+		finish_reason = try values.decodeIfPresent(String.self, forKey: .finish_reason)
+	}
+
+}
