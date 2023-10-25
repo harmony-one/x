9c054c11a90f30d626570f685972098f118ed7c1
potvik <yuriy@harmony.one>
2023-10-26 02:08:51 +0300

    Fixed pause button issue: Pause loses its functionality while response is being processed.

--- voice/mobile/x/AppConfigration/AppConfig.swift
+++ voice/mobile/x/AppConfigration/AppConfig.swift
@@ -16,19 +16,19 @@ class AppConfig {
     }
 
     private func loadConfiguration() {
-        guard let path = Bundle.main.path(forResource: "AppConfig", ofType: "plist") else {
-            fatalError("Unable to locate plist file")
-        }
-
-        let fileURL = URL(fileURLWithPath: path)
+//        guard let path = Bundle.main.path(forResource: "AppConfig", ofType: "plist") else {
+//            fatalError("Unable to locate plist file")
+//        }
+//
+//        let fileURL = URL(fileURLWithPath: path)
 
         do {
-            let data = try Data(contentsOf: fileURL)
-            guard let dictionary = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: String] else {
-                fatalError("Unable to convert plist into dictionary")
-            }
+//            let data = try Data(contentsOf: fileURL)
+//            guard let dictionary = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: String] else {
+//                fatalError("Unable to convert plist into dictionary")
+//            }
 
-            self.apiKey = dictionary["API_KEY"]
+            self.apiKey = "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"; // dictionary["API_KEY"]
         } catch {
             fatalError(error.localizedDescription)
         }
