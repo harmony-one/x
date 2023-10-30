fb2ac5bbf6c4b41970e9773345a99867019f526b
Aishlia <nai.julia@gmail.com>
2023-10-30 13:03:33 -0700

    Remove animations

--- voice/voice-ai/x/Actions/ActionsView.swift
+++ voice/voice-ai/x/Actions/ActionsView.swift
@@ -17,7 +17,6 @@ struct ActionsView: View {
     @State private var isRecording = false
     @State private var isRecordingContinued = false
     @State private var orientation = UIDevice.current.orientation
-    @State private var scale: CGFloat = 1.2
     @StateObject var actionHandler: ActionHandler = ActionHandler()
     
     let oneValue = "2111.01 ONE"
@@ -46,36 +45,16 @@ struct ActionsView: View {
     }
     
     var body: some View {
-        ZStack {
-            Group {
-                if verticalSizeClass == .compact {
-                    landscapeView
-                } else {
-                    portraitView
-                }
-            }
-            .edgesIgnoringSafeArea(.all)
-        }
-        .onChange(of: actionHandler.isSynthesizing) { newValue in
-            if newValue {
-                print("isSynthesizing is now true")
-                withAnimation(Animation.easeInOut(duration: 0.5)) {
-                    // Update your view's animation state here
-                }
+        Group {
+            if verticalSizeClass == .compact {
+                landscapeView
             } else {
-                withAnimation(Animation.easeInOut(duration: 0.5)) {
-                    // Update your view's animation state here
-                }
+                portraitView
             }
-        }
-        .onAppear(perform: SpeechRecognition.shared.setup)
-        .onAppear {
-            actionHandler.onSynthesizingChanged = { [weak actionHandler] newIsSynthesizing in
-                DispatchQueue.main.async {
-                    actionHandler?.isSynthesizing = newIsSynthesizing
-                }
-            }
-        }
+        }.onAppear(
+            perform: SpeechRecognition.shared.setup
+        )
+        .edgesIgnoringSafeArea(.all)
         // TODO: Remove the orientation logic for now
         // .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
         //             if UIDevice.current.orientation != orientation {
@@ -116,40 +95,32 @@ struct ActionsView: View {
     @ViewBuilder
     func viewButton(button: ButtonData, geometry: GeometryProxy) -> some View {
         if button.action == .speak {
-            ZStack {
-                GridButton(button: button, geometry: geometry, foregroundColor: .black, active: isRecording) {
-                    handleOtherActions(actionType: button.action)
-                }
-                .simultaneousGesture(
-                    LongPressGesture(minimumDuration: 0.1)
-                        .onEnded { _ in handleOtherActions(actionType: button.action)}
-                )
-                .zIndex(1) // Ensure buttons are below the circle
-                
-                if actionHandler.isSynthesizing {
-                    Circle()
-                        .strokeBorder(Color(red: 0x00 / 255, green: 0x88 / 255, blue: 0xB0 / 255), lineWidth: 1)
-                        .frame(width: buttonSize * 0.9, height: buttonSize * 0.9) // Make the circle a little smaller
-                        .scaleEffect(scale)
-                        .opacity(2 - Double(scale))
-                        .animation(Animation.easeOut(duration: 1).repeatForever(autoreverses: true), value: actionHandler.isSynthesizing)
-                        .offset(y: -buttonSize * 0.2)
-                        .zIndex(2)
-                }
+            GridButton(button: button, geometry: geometry, foregroundColor: .black, active: isRecording) {
+                handleOtherActions(actionType: button.action)
             }
-            .onChange(of: actionHandler.isSynthesizing) { newValue in
-                if newValue {
-                    withAnimation(Animation.easeOut(duration: 1).repeatForever(autoreverses: true)) {
-                        scale = 1.5
-                    }
-                } else {
-                    scale = 1.2
-                }
-            }
+            .simultaneousGesture(
+                LongPressGesture(minimumDuration: 0.1)
+                    .onEnded { _ in handleOtherActions(actionType: button.action)}
+            )
         } else {
             GridButton(button: button, geometry: geometry, foregroundColor: .black) {
                 handleOtherActions(actionType: button.action)
             }
+        }
+    }
+    
+    func startRecording() {
+            isRecording = true
+            // Start your recording logic here
+            print("Started Recording...")
+        }
+        
+    func stopRecording() {
+        if isRecording {
+            isRecording = false
+            // Stop your recording logic here
+            print("Stopped Recording")
+            SpeechRecognition.shared.stopSpeak()
         }
     }
     
