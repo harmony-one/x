f1e15f9d9a114c7185ee5e2cb4c9e6fadcb109c4
Aishlia <nai.julia@gmail.com>
2023-10-31 15:05:28 -0700

    Add testing for Permission.swift. Test coverage 27% -> 28%

---
+++ voice/voice-ai/xTests/PermissionTest.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import Voice_AI
+
+class PermissionTests: XCTestCase {
+    
+    func testRequestMicrophoneAccess_Granted() {
+        let permission = Permission()
+        let expectation = self.expectation(description: "Microphone access granted")
+        
+        permission.requestMicrophoneAccess { granted in
+            XCTAssertTrue(granted)
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testCheckMicrophoneAccess_Granted() {
+        let permission = Permission()
+        XCTAssertTrue(permission.checkMicrophoneAccess())
+    }
+}
