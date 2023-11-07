fbf2611919a0b922b5bb42ad466bef2020a180a6
fegloff <fegloff@gmail.com>
2023-11-07 10:36:55 -0500

    remove commented code

--- voice/voice-ai/x/ThemeManager/ThemeManager.swift
+++ voice/voice-ai/x/ThemeManager/ThemeManager.swift
@@ -11,13 +11,13 @@ import SwiftUI
 
 enum ThemeName {
     case herTheme
-    case darkTheme
+    case blackredTheme
     case defaultTheme
 
     static func fromString(_ name: String) -> ThemeName {
         switch name {
         case "herTheme": return .herTheme
-        case "darkTheme": return .darkTheme
+        case "blackredTheme": return .blackredTheme
         case "defaultTheme": return .defaultTheme
         default: return .defaultTheme
         }
@@ -26,22 +26,12 @@ enum ThemeName {
 
 class Theme:ObservableObject {
     @Published var name: ThemeName
-//    @Published var brandColor: Color
-//    @Published var backgroundColor: Color
-//    @Published var contrastBackgroundColor: Color
-//    @Published var secondaryColor: Color
-//    @Published var shadowColor: Color
     @Published var bodyTextColor: Color
     @Published var buttonActiveColor: Color
     @Published var buttonDefaultColor: Color
 
     init(name: ThemeName, bodyTextColor:Color, buttonActiveColor: Color, buttonDefaultColor: Color){
         self.name = name
-//        self.brandColor = brandColor
-//        self.backgroundColor = backgroundColor
-//        self.contrastBackgroundColor = contrastBackgroundColor
-//        self.secondaryColor = secondaryColor
-//        self.shadowColor = shadowColor
         self.bodyTextColor = bodyTextColor
         self.buttonActiveColor = buttonActiveColor
         self.buttonDefaultColor = buttonDefaultColor
@@ -61,23 +51,13 @@ struct ThemeManager {
     var themes: [Theme] = [
         Theme(
             name: .defaultTheme,
-//            brandColor: Color(hex: 0x552233),
-//            backgroundColor: Color(hex: 0x552233),
-//            contrastBackgroundColor: Color(hex: 0x552233),
-//            secondaryColor: Color(hex: 0x552233),
-//            shadowColor: Color(hex: 0x552233),
             bodyTextColor: Color(hex: 0x552233),
             buttonActiveColor: Color(hex: 0x0088B0),
             buttonDefaultColor: Color(hex: 0xDDF6FF)
         ),
         
         Theme(
-            name: .herTheme,
-//            brandColor: Color(hex: 0x552233),
-//            backgroundColor: Color(hex: 0x552233),
-//            contrastBackgroundColor: Color(hex: 0x552233),
-//            secondaryColor: Color(hex: 0x552233),
-//            shadowColor: Color(hex: 0x552233),
+            name: .blackredTheme,
             bodyTextColor: Color(hex: 0x552233),
             buttonActiveColor: Color(hex: 0xb02c00),
             buttonDefaultColor: Color(hex: 0xc9785d)
@@ -88,3 +68,4 @@ struct ThemeManager {
         return themes.first { $0.name == name } ?? themes[0]
     }
 }
+
