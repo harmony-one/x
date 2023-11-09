59d64b380a29bfb9d68dbdec27e4e33a4f1f0f79
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2023-11-09 11:25:24 -0800

    change tap icon

--- voice/voice-ai/x/Actions/ActionsView.swift
+++ voice/voice-ai/x/Actions/ActionsView.swift
@@ -54,7 +54,7 @@ struct ActionsView: View {
         let buttonReset = ButtonData(label: "New Session", image: "\(themePrefix) - new session", action: .reset)
 //        let buttonSayMore = ButtonData(label: "Say More", image: "\(themePrefix) say more", action: .sayMore)
 //        let buttonUserGuide = ButtonData(label: "User Guide", image: "\(themePrefix) - user guide", action: .userGuide)
-        let buttonTapSpeak = ButtonData(label: "Tap to Speak", pressedLabel: "Tap to Send", image: "\(themePrefix) - press & hold", action: .speak)
+        let buttonTapSpeak = ButtonData(label: "Tap to Speak", pressedLabel: "Tap to Send", image: "\(themePrefix) - square", action: .speak)
         let buttonSurprise = ButtonData(label: "Surprise ME!", image: "\(themePrefix) - random fact", action: .surprise)
         let buttonSpeak = ButtonData(label: "Press & Hold", image: "\(themePrefix) - press & hold", action: .speak)
         let buttonRepeat = ButtonData(label: "Repeat Last", image: "\(themePrefix) - repeat last", action: .repeatLast)
