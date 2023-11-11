ba78a8f047eb11693abe41b0bd72ad9ad5480fdb
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2023-11-11 14:54:56 -0800

    setup husky prehook

--- .husky/pre-commit-script.sh
+++ .husky/pre-commit-script.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+# Check for AppConfig.plist
 if git diff --cached --name-only | grep --quiet "AppConfig.plist"; then
   echo "Error: Commit contains AppConfig.plist, which is not allowed."
   exit 1
 fi
+
