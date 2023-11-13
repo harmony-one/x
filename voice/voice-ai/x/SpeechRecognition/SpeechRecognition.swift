1b3b287c4bfa2e561ce612b00f17fbb624907356
Rika <15hatoyar@gmail.com>
2023-11-13 15:46:19 -0800

    Added more beep beeps

--- voice/voice-ai/x/SpeechRecognition/SpeechRecognition.swift
+++ voice/voice-ai/x/SpeechRecognition/SpeechRecognition.swift
@@ -219,6 +219,7 @@ class SpeechRecognition: NSObject, ObservableObject, SpeechRecognitionProtocol {
             
             if recognitionTaskCanceled != true && nsError.domain == "kAFAssistantErrorDomain" && nsError.code == 1110 {
                 print("No speech was detected. Please speak again.")
+                audioPlayer.playSound(false)
 //                self.registerTTS()
 //                self.textToSpeechConverter.convertTextToSpeech(text: "Say again.")
                 return
@@ -396,6 +397,7 @@ class SpeechRecognition: NSObject, ObservableObject, SpeechRecognitionProtocol {
                     handleQuery(retryCount: retryCount - 1)
                 }
             } else {
+                audioPlayer.playSound(false)
                 SentrySDK.capture(message: "[SpeechRecognition] OpenAI error: \(nsError). No more retries.")
                 print("[SpeechRecognition] OpenAI error: \(nsError). No more retries.")
                 buf.removeAll()
