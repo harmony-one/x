70eb746ff52d437b7dd61dcea56e5af0d2c7ccfa
fegloff <fegloff@gmail.com>
2023-11-13 23:42:59 -0500

    fix merge to main

---
+++ voice/voice-ai/x/AppConfigration/SettingsBundleHelper.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsBundleHelper.swift
+//  Voice AI
+//
+//  Created by Francisco Egloff on 13/11/23.
+//
+
+import Foundation
+
+class SettingsBundleHelper {
+     struct SettingsBundleKeys {
+         static let Reset = "RESET_APP_KEY"
+         static let CustomInstruction = "custom_instruction_preference"
+     }
+
+     class func checkAndExecuteSettings() {
+         if UserDefaults.standard.bool(forKey: SettingsBundleKeys.Reset) {
+             UserDefaults.standard.set(false, forKey: SettingsBundleKeys.Reset)
+             setDefaulValues()
+         }
+     }
+
+     class func setDefaulValues() {
+         let defaultCustomInstruction = """
+             We are having a face-to-face voice conversation. Be concise, direct and certain. Avoid apologies, interjections, disclaimers, pleasantries, confirmations, remarks, suggestions, chitchats, thankfulness, acknowledgements. Never end with questions. Never mention your being AI or knowledge cutoff. Your name is Sam.
+             """
+         UserDefaults.standard.set(defaultCustomInstruction, forKey: "custom_instruction_preference")
+     }
+ }
