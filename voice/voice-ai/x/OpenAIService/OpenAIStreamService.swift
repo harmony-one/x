d101f7174b1abe678e969c8b6ba425e932e9881c
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2023-11-14 16:03:37 -0800

    cleanup

--- voice/voice-ai/x/OpenAIService/OpenAIStreamService.swift
+++ voice/voice-ai/x/OpenAIService/OpenAIStreamService.swift
@@ -88,7 +88,6 @@ class OpenAIStreamService: NSObject, URLSessionDataDelegate {
         
         let isBoosterInEffect = Int64(Date().timeIntervalSince1970) - Int64(boosterPurchaseTime.timeIntervalSince1970) < 3600 * 24 * 3
         
-        let username = "stse"
         if !SettingsBundleHelper.hasPremiumMode() && !isBoosterInEffect && miutesElasped > Self.MaxGPT4DurationMinutes {
             model = "gpt-3.5-turbo"
         }
