d78c737049c25a52bf91e4c6a375e5ab76e1637f
Rika <15hatoyar@gmail.com>
2023-11-21 11:52:02 -0800

    initial

--- voice/voice-ai/xTests/ActionsViewTests_rika.swift
+++ voice/voice-ai/xTests/ActionsViewTests_rika.swift
@@ -1,8 +1,58 @@
-//
-//  ActionsViewTests_rika.swift
-//  Voice AITests
-//
-//  Created by Rikako Hatoya on 11/21/23.
-//
+import XCTest
+@testable import Voice_AI
+class ActionsViewTests: XCTestCase {
+    var actionsView: ActionsView!
 
-import Foundation
+    override func setUp() {
+        super.setUp()
+        actionsView = ActionsView()
+    }
+
+    override func tearDown() {
+        actionsView = nil
+        super.tearDown()
+    }
+
+    func testInitialization() {
+        // Test the initialization of the ActionsView instance
+        XCTAssertNotNil(actionsView)
+        // You can add more assertions for properties and states here
+    }
+
+    func testChangeTheme() {
+        let themeName = "blackredTheme"
+        actionsView.changeTheme(name: themeName)
+        // Verify that the theme has been changed correctly, you can assert on properties or states
+    }
+//
+//    func testVibration() {
+//        // Test the vibration function
+//        // You may need to mock or stub certain dependencies to fully test this function
+//        // For example, you can use XCTestExpectation to wait for an expected vibration
+//    }
+//
+//    func testOpenSettingsApp() {
+//        // Test the openSettingsApp function
+//        // You may need to mock or stub UIApplication functions to fully test this
+//    }
+//
+//    func testHandleOtherActions() async {
+//        // Test the handleOtherActions function
+//        // You may need to mock or stub certain dependencies to fully test this function
+//    }
+//
+//    func testCheckUserAuthentication() {
+//        // Test the checkUserAuthentication function
+//        // You may need to mock or stub certain dependencies to fully test this function
+//    }
+//
+//    func testShowPurchaseDialog() {
+//        // Test the showPurchaseDialog function
+//        // You may need to mock or stub certain dependencies to fully test this function
+//    }
+//
+//    func testShowInAppPurchasesIfNotLoggedIn() {
+//        // Test the showInAppPurchasesIfNotLoggedIn function
+//        // You may need to mock or stub certain dependencies to fully test this function
+//    }
+}
