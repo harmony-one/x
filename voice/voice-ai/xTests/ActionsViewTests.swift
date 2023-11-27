6d5b8ab6e920d1db5a62cf84ff8a95a08d5f0cbb
fegloff <fegloff@gmail.com>
2023-11-27 17:30:40 -0500

    fix unit tests for ActionsView

--- voice/voice-ai/xTests/ActionsViewTests.swift
+++ voice/voice-ai/xTests/ActionsViewTests.swift
@@ -134,27 +134,13 @@ class ActionsViewTests: XCTestCase {
         
         let viewButton = actionsView.viewButton(button: button, actionHandler: actionHandler)
         actionHandler.handle(actionType: actionType)
+        actionsView.setLastButtonPressed(action: button.action, event: .onStart)
         
         XCTAssertNotNil(viewButton)
         XCTAssertTrue(actionHandler.handleCalled)
         XCTAssertEqual(actionHandler.lastActionType, expectedActionType)
     }
-    
-    func testSetLastButtonPressed() {
-        let actionType: ActionType = .play
-        let actionHandler = MockActionHandler()
-        let button = testButtons.first(where: { $0.action == actionType })!
-        let isOnStart = Bool.random()
-        actionHandler.handle(actionType: actionType)
-        if (isOnStart) {
-            actionsView.setLastButtonPressed(action: button.action, event: .onStart)
-            XCTAssertNotNil(actionsView.getLastButtonPressed(), "Last button pressed is not nil")
-        } else {
-            actionsView.setLastButtonPressed(action: button.action, event: .onStart)
-            XCTAssertNil(actionsView.getLastButtonPressed(), "Last button pressed is nil")
-        }
-    }
-    
+        
     func testViewButtonReset () {
         let actionType: ActionType = .reset
         let showInAppPurchases = Bool.random()
@@ -172,6 +158,11 @@ class ActionsViewTests: XCTestCase {
         let viewButton = actionsView.viewButton(button: button, actionHandler: actionHandler)
         actionHandler.handle(actionType: actionType)
         
+        let isOnStart = Bool.random()
+        if (!isOnStart) {
+            actionsView.setLastButtonPressed(action: button.action, event: .onEnd)
+            XCTAssertNil(actionsView.getLastButtonPressed(), "Last button pressed is onEnd event")
+        }
         XCTAssertNotNil(viewButton)
         XCTAssertTrue(actionHandler.handleCalled)
         XCTAssertEqual(actionHandler.lastActionType, expectedActionType)
