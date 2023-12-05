17396077f1c767298d73017e858eff7b4b3ebaa6
Aishlia <nai.julia@gmail.com>
2023-12-05 10:45:31 -0800

    More comprehensive CreateeUserTests.swift

--- voice/voice-ai/xTests/CreateUserTests.swift
+++ voice/voice-ai/xTests/CreateUserTests.swift
@@ -35,21 +35,70 @@ class UserTests: XCTestCase {
         }
     }
 
-//    func testDecodingPartialData() {
-//        // Similar to the above but with missing fields in the JSON
-//    }
-//
-//    func testDecodingInvalidData() {
-//        // Provide JSON with incorrect data types and assert an error is thrown
-//    }
+    // Similar to the above but with missing fields in the JSON
+    func testDecodingPartialData() {
+        let json = """
+        {
+            "id": "123",
+            "deviceId": "device123",
+            "appleId": "apple123",
+            "createdAt": "2021-01-01T00:00:00Z",
+            "updatedAt": "2021-01-02T00:00:00Z",
+            "expirationDate": "2022-01-01T00:00:00Z",
+            "appVersion": "1.0.0"
+        }
+        """.data(using: .utf8)!
+        
+        do {
+            let user = try JSONDecoder().decode(User.self, from: json)
+            print(user)
+            XCTAssertEqual(user.id, "123")
+            XCTAssertEqual(user.deviceId, "device123")
+            XCTAssertEqual(user.appleId, "apple123")
+            XCTAssertEqual(user.balance, nil)
+            XCTAssertEqual(user.createdAt, "2021-01-01T00:00:00Z")
+            XCTAssertEqual(user.updatedAt, "2021-01-02T00:00:00Z")
+            XCTAssertEqual(user.expirationDate, "2022-01-01T00:00:00Z")
+            XCTAssertEqual(user.isSubscriptionActive, nil)
+            XCTAssertEqual(user.appVersion, "1.0.0")
+        } catch {
+            XCTFail("Decoding failed: \(error)")
+        }
+        
+    }
 
-//    func testDecodingEmptyData() {
-//        let json = "{}".data(using: .utf8)!
-//        do {
-//            let user = try JSONDecoder().decode(User.self, from: json)
-//              Assert default or nil values
-//        } catch {
-//            XCTFail("Decoding failed: \(error)")
-//        }
-//    }
+    // Similar as above but with invalid data
+    func testDecodingInvalidData() {
+        let json = """
+        {
+            "id": "123",
+            "deviceId": ".*",
+            "appleId": ".*",
+            "balance": -1,
+            "createdAt": "2021-01-01T00:Z",
+            "updatedAt": "2021-01-0=Z",
+            "expirationDate": "2022-01-01T00:00:00Z",
+            "isSubscriptionActive": bar,
+            "appVersion": "-1"
+        }
+        """.data(using: .utf8)!
+
+        do {
+            let _ = try JSONDecoder().decode(User.self, from: json)
+            XCTFail("Decoding should fail with invalid data")
+        } catch {
+        }
+    }
+
+    // Similar as above but with no data
+    func testDecodingEmptyData() {
+        let json = "{}".data(using: .utf8)!
+        do {
+            let user = try JSONDecoder().decode(User.self, from: json)
+            print(user)
+              //Assert default or nil values
+        } catch {
+            XCTFail("Decoding failed: \(error)")
+        }
+    }
 }
