009227df3edf346f1d4bc35ee1c3d1da8650f3c8
Aishlia <nai.julia@gmail.com>
2023-12-07 17:39:12 -0800

    PEP8-ing sentry_data

--- voice/analytics/sentry_data.py
+++ voice/analytics/sentry_data.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 import os
-load_dotenv()
 import requests
 from datetime import datetime, timedelta
 
-# Sentry API endpoint
-sentry_api_base_url = "https://sentry.io/api/0"
+# Load environment variables
+load_dotenv()
 
-# Your Sentry API key
+# Sentry API endpoint and API key
+sentry_api_base_url = "https://sentry.io/api/0"
 api_key = os.getenv('SENTRY_TOKEN')
 
 # Headers for authentication
@@ -15,15 +15,14 @@ headers = {
     "Authorization": f"Bearer {api_key}"
 }
 
+
 def get_prod_error_count(organization_slug, project_slug):
+    """Get production error count from Sentry."""
     now = datetime.utcnow()
     start_time = now - timedelta(days=1)
     start_time_str = start_time.strftime('%Y-%m-%dT%H:%M:%S')
-
     query = '!event.type:transaction !message:"handleRecognitionError: "'
-
     endpoint = f"{sentry_api_base_url}/projects/{organization_slug}/{project_slug}/issues/"
-
     params = {
         'query': query,
         'statsPeriod': '24h',
@@ -32,26 +31,21 @@ def get_prod_error_count(organization_slug, project_slug):
 
     response = requests.get(endpoint, headers=headers, params=params)
     if response.status_code == 200:
-        data = response.json()
-        return len(data)
+        return len(response.json())
     else:
-        return f"Error: {response.status_code}"
+        raise Exception(f"Error: {response.status_code}")
 
 
 def get_project_issues(organization_slug, project_slug):
+    """Get project issues from Sentry."""
     endpoint = f"{sentry_api_base_url}/projects/{organization_slug}/{project_slug}/issues/"
     response = requests.get(endpoint, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
-        return f"Error: {response.status_code}"
+        raise Exception(f"Error: {response.status_code}")
 
 
-organization_slug = "harmony-23"
-project_slug = "apple-ios"
-
-# issues = get_project_issues(organization_slug, project_slug)
-# print(issues)
 if __name__ == "__main__":
     organization_slug = "harmony-23"
     project_slug = "apple-ios"
