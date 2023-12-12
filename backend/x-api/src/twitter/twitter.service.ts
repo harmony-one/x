fbd517dc25fc4603733b9e188757bb7180ce9acc
artemkolodko <temakolodko@gmail.com>
2023-12-12 18:53:09 +0000

    Add axios

--- backend/x-api/src/twitter/twitter.service.ts
+++ backend/x-api/src/twitter/twitter.service.ts
@@ -1,14 +1,18 @@
 import {Injectable, Logger} from '@nestjs/common';
 import {ConfigService} from "@nestjs/config";
+import axios from 'axios'
 
 @Injectable()
 export class TwitterService {
   logger = new Logger(TwitterService.name)
   constructor(private readonly configService: ConfigService) {}
 
-  public getTwitterFeed() {
+  public async getTwitterFeed() {
     const twitterUsername = this.configService.get('twitter.username')
     this.logger.log(`Twitter username: ${twitterUsername}`)
+
+    // const { data } = await axios.get('SomeRequestURL')
+
     return 'test'
   }
 }
