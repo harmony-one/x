67cdd4a7845e5a2476f2a4c24b0c5244b605de6d
artemkolodko <temakolodko@gmail.com>
2023-12-13 14:35:55 +0000

    Add basic implementation of GET /twitter/list endpoint

--- backend/x-api/src/twitter/twitter.controller.ts
+++ backend/x-api/src/twitter/twitter.controller.ts
@@ -1,14 +1,15 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, Query} from '@nestjs/common';
 import {ApiTags} from "@nestjs/swagger";
 import {TwitterService} from "./twitter.service";
+import {GetListTweetsDto} from "./dto/list.dto";
 
 @ApiTags('twitter')
 @Controller('twitter')
 export class TwitterController {
   constructor(private readonly twitterService: TwitterService) {}
 
-  @Get('/feed')
-  getFeed() {
-    return this.twitterService.getTwitterFeed()
+  @Get('/list')
+  getListTweets(@Query() dto: GetListTweetsDto) {
+    return this.twitterService.getListTweets(dto)
   }
 }
