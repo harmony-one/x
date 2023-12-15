b9d3c213e9931c3925b2bb0571122c49ad14e69b
Nagesh Kumar Mishra <nagesh.mishra@fissionlabs.com>
2023-12-15 20:55:41 +0530

    Fixed asynchronous issue

--- voice/voice-ai/x/Logs/LogStore.swift
+++ voice/voice-ai/x/Logs/LogStore.swift
@@ -5,14 +5,14 @@ import SwiftUI
 @MainActor
 final class LogStore: ObservableObject {
     @Published private(set) var entries: [String] = []
-    
+
     public let logger = Logger(
         subsystem: Bundle.main.bundleIdentifier!,
         category: String(describing: LogStore.self)
     )
-    
-    func export() {
-        print("[LogStore][export] Exporting Logs")
+
+    // Mark this function as asynchronous
+    func performExport() async throws -> [String] {
         let dateFormatStyle = Date.FormatStyle()
             .year(.defaultDigits)
             .month(.twoDigits)
@@ -21,37 +21,37 @@ final class LogStore: ObservableObject {
             .minute(.twoDigits)
             .second(.twoDigits)
             .secondFraction(.fractional(3))
-        
-        DispatchQueue.global(qos: .userInitiated).async {
+
+        let store = try OSLogStore(scope: .currentProcessIdentifier)
+        let position = store.position(timeIntervalSinceLatestBoot: 1)
+
+        // Assuming fetching entries might involve asynchronous operations
+        let entries = try await fetchLogEntries(store: store, position: position, dateFormatStyle: dateFormatStyle)
+        return entries
+    }
+
+    private func fetchLogEntries(store: OSLogStore, position: OSLogPosition, dateFormatStyle: Date.FormatStyle) async throws -> [String] {
+        // Perform the actual log entries fetching asynchronously
+        // For now, we just use a synchronous method for illustration
+        return try store
+            .getEntries(at: position)
+            .compactMap { $0 as? OSLogEntryLog }
+            .filter { $0.subsystem == Bundle.main.bundleIdentifier! }
+            .map { "[\($0.date.formatted(dateFormatStyle))] \($0.composedMessage)" }
+            .reversed()
+    }
+
+    func exportInBackground(completion: @escaping () -> Void) {
+        Task {
             do {
-                let store = try OSLogStore(scope: .currentProcessIdentifier)
-                let position = store.position(timeIntervalSinceLatestBoot: 1)
-                let fetchedEntries = try store
-                    .getEntries(at: position)
-                    .compactMap { $0 as? OSLogEntryLog }
-                    .filter { $0.subsystem == Bundle.main.bundleIdentifier! }
-                    .map { "[\($0.date.formatted(dateFormatStyle))] \($0.composedMessage)" }
-                
-                // Convert the ReversedCollection back to an Array
-                let reversedEntries = Array(fetchedEntries.reversed())
+                let fetchedEntries = try await performExport()
                 DispatchQueue.main.async {
-                    self.entries = reversedEntries
+                    self.entries = Array(fetchedEntries)
+                    completion()
                 }
             } catch {
                 DispatchQueue.main.async {
                     self.logger.warning("\(error.localizedDescription, privacy: .public)")
-                }
-            }
-        }
-    }
-    
-    // Method to be called from the background
-    func exportInBackground(completion: @escaping () -> Void) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            // Perform heavy operation in the background
-            Task {
-                await self.export()
-                DispatchQueue.main.async {
                     completion()
                 }
             }
